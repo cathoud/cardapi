c8d726c4efca0a86c18fb3c8353c8a85bbc64f82
Cathoud <12943365+cathoud@users.noreply.github.com>
2022-06-20 15:30:29 -0300

    Add jdbc

---
+++ src/main/java/com/amecardsapi/config/DatabaseConfig.java
@@ -0,0 +1,37 @@
+package com.amecardsapi.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Configuration
+public class DatabaseConfig {
+
+    private static final Logger LOG = LogManager.getLogger(DatabaseConfig.class);
+    @Value("${database.url}")
+    private String url;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
--- src/main/java/com/amecardsapi/repository/impl/CardRepositoryImpl.java
+++ src/main/java/com/amecardsapi/repository/impl/CardRepositoryImpl.java
@@ -1,39 +1,94 @@
 package com.amecardsapi.repository.impl;
 
-import com.amecardsapi.exception.InvalidEntityAttributeException;
+import com.amecardsapi.exception.ApplicationException;
 import com.amecardsapi.model.Card;
 import com.amecardsapi.repository.CardRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
 import java.util.Optional;
 
 @Repository
 public class CardRepositoryImpl implements CardRepository {
 
-    private final List<Card> cards = new ArrayList<>();
+    private static final Logger LOG = LogManager.getLogger(CardRepositoryImpl.class);
+    private final ConnectionFactory connectionFactory;
+    @Autowired
+    public CardRepositoryImpl(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
 
     @Override
     public Optional<Card> findById(int id) {
-        return cards.stream()
-                .filter(card -> card.getId() == id)
-                .findFirst();
+        String query = "SELECT * FROM card WHERE id = ?";
+
+        try(Connection connection = connectionFactory.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setInt(1, id);
+                statement.execute();
+
+                ResultSet resultSet = statement.getResultSet();
+                if(resultSet.next()) {
+                    Card card = new Card();
+                    card.setId(resultSet.getInt("id"));
+                    card.setName(resultSet.getString("name"));
+                    card.setDescription(resultSet.getString("description"));
+                    card.setImageUrl(resultSet.getString("image_url"));
+                    card.setStrength(resultSet.getInt("strength"));
+                    card.setSpeed(resultSet.getInt("speed"));
+                    card.setSkill(resultSet.getInt("skill"));
+                    card.setGear(resultSet.getInt("gear"));
+                    card.setIntellect(resultSet.getInt("intellect"));
+                    card.setOriginId(resultSet.getInt("originId"));
+                    card.setCreatedAt(resultSet.getTimestamp("created_at").toLocalDateTime());
+                    card.setUpdatedAt(resultSet.getTimestamp("updated_at").toLocalDateTime());
+
+                    return Optional.of(card);
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("{}", e.getMessage());
+        }
+
+        return Optional.empty();
     }
 
     @Override
     public Card save(Card card) {
-        var cardFound = cards.stream()
-                .filter(storedCard -> storedCard.getName().equals(card.getName()))
-                .findFirst();
+        String insertQuery = "INSERT INTO card (name, description, image_url, strength, speed, skill, gear, intellect, created_at, updated_at, originId)" +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?)";
 
-        if(cardFound.isPresent()) {
-            throw new InvalidEntityAttributeException("Name [" + card.getName() + "] already stored");
+        try(Connection connection = connectionFactory.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS)) {
+                statement.setString(1, card.getName());
+                statement.setString(2, card.getDescription());
+                statement.setString(3, card.getImageUrl());
+                statement.setInt(4, card.getStrength());
+                statement.setInt(5, card.getSpeed());
+                statement.setInt(6, card.getSkill());
+                statement.setInt(7, card.getGear());
+                statement.setInt(8, card.getIntellect());
+                statement.setTimestamp(9, Timestamp.valueOf(card.getCreatedAt()));
+                statement.setTimestamp(10, Timestamp.valueOf(card.getUpdatedAt()));
+                statement.setInt(11, (int) card.getOriginId());
+
+                statement.execute();
+
+                ResultSet resultSet = statement.getGeneratedKeys();
+                if(resultSet.next()) {
+                    card.setId(resultSet.getInt(1));
+
+                    return card;
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("{}", e.getMessage());
+            throw new ApplicationException(e.getMessage());
         }
 
-        card.setId(cards.size() + 1);
-        cards.add(card);
-
-        return card;
+        throw new ApplicationException("Could not save card " + card );
     }
 }
